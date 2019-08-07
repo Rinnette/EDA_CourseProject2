3e46b9189b77b0f67cfe1a3ebf10b46d14a8568b
Rinnette Ramdhanie <rinnette@gmail.com>
2019-08-06 22:39:43 -0400

    Create barplot showing total PM 2.5 emissions

---
+++ plot1.R
@@ -0,0 +1,22 @@
+# Question 1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008
+
+
+# Read data 
+        NEI <- readRDS("summarySCC_PM25.rds")
+        SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sum PM 2.5 Emissions for each year and save in sumEM        
+        sumEm <- aggregate(Emissions ~ year, NEI, sum)
+        
+
+# Create barplot and save to PNG file
+        png("plot1.png", width = 480, height = 480)
+        
+        barplot(sumEm$Emissions, sumEm$year, names.arg = sumEm$year, col = "blue", 
+                main = "Total PM 2.5 Emissions", ylim = c(0, 8000000), xlab = "Year", 
+                ylab = "Sum of PM2.5 Emissions")
+        
+        dev.off()
+
+
+        
