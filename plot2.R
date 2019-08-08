e3aded5a9ea3f280facd052523a00679e16d0c90
Rinnette Ramdhanie <rinnette@gmail.com>
2019-08-07 22:53:28 -0400

    Edit labels of plot 2

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-# Question 1: Have total emissions from PM2.5 decreased in Baltimore City, Maryland from 1999 to 2008
+# Question 2: Have total emissions from PM2.5 decreased in Baltimore City, Maryland from 1999 to 2008
 
 
 # Read data 
@@ -14,8 +14,8 @@
         png("plot2.png", width = 480, height = 480)
         
         barplot(sumBC$Emissions, sumBC$year, names.arg = sumBC$year, col = "orange", 
-                main = "Total PM 2.5 Emissions in Baltimore City", xlab = "Year", 
-                ylab = "Sum of PM2.5 Emissions", ylim = c(0, 3500))
+                main = expression("Total PM"[2.5] * " Emissions in Baltimore City"), xlab = "Year", 
+                ylab = expression(PM[2.5] * " Emissions (tons)"), ylim = c(0, 3500))
         
         dev.off()
 
