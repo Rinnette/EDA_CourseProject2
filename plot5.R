1d32da20dd419f4594704198a41f4e71eef002c9
Rinnette Ramdhanie <rinnette@gmail.com>
2019-08-09 23:41:13 -0400

    Create plot of emissions from motor vehicle sources in Baltimore City

---
+++ plot5.R
@@ -0,0 +1,21 @@
+# Question 5:   
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+
+
+# Read data 
+        NEI <- readRDS("summarySCC_PM25.rds")
+        SCC <- readRDS("Source_Classification_Code.rds")
+
+# Get data required for plot
+        BConroad <- subset(NEI, fips == "24510" & type == "ON-ROAD")
+        plotdata <- aggregate(Emissions ~ year, BConroad, sum)
+        
+# Create plot and save to PNG file
+        library(ggplot2)
+        ggplot(plotdata, aes(factor(year), Emissions)) +
+                geom_bar(stat = "identity", fill = "#a3b6d6") +
+                xlab("Year") +
+                ylab(expression(PM[2.5] * " Emissions (tons)")) +
+                ggtitle(expression(PM[2.5] * " Emissions from Motor Vehicle Sources in Baltimore City"))
+        
+        ggsave("plot5.png")
